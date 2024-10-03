9212933c9362de34e10bc446c7d85edbe20f3c1b
Big Ike <bigike@werespecialstudios.com>
2024-10-02 22:48:10 -0500

    Created building lists w/ percent ownership for each nation

--- un_project/un_app/urls.py
+++ un_project/un_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import building_list, player_list, home
+from .views import building_list, player_list, home, nation_building_list
 
 urlpatterns = [
     path('', home, name='home'),  # Default home page
     path('buildings/', building_list, name='building_list'),  # http://127.0.0.1:8000/buildings/
-    path('players/', player_list, name='player_list')         # http://127.0.0.1:8000/players/
+    path('players/', player_list, name='player_list'),        # http://127.0.0.1:8000/players/
+    path('nation/<str:nation_abbreviation>/', nation_building_list, name='nation_building_list'),  # http://127.0.0.1:8000/nation/XYZ/
 ]
--- un_project/un_app/views.py
+++ un_project/un_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.db.models import F, FloatField, ExpressionWrapper, Count, Value
+from django.shortcuts import render, get_object_or_404
+from django.db.models import F, FloatField, ExpressionWrapper, Count, Value, Sum
 from django.db.models.functions import Coalesce
-from .models import Building, Player
+from .models import Building, Player, Nation, PartialBuildingOwnership
 
 def home(request):
     return render(request, 'home.html')
@@ -25,3 +25,22 @@ def player_list(request):
         num_buildings_built=Count('main_builds')
     ).order_by('-num_buildings_built')  # Sort by the number of buildings built in descending order
     return render(request, 'player_list.html', {'players': players})
+
+
+def nation_building_list(request, nation_abbreviation):
+    # Get the nation by its abbreviation
+    nation = get_object_or_404(Nation, abbreviation=nation_abbreviation)
+    
+    # Get all buildings owned by the nation
+    buildings = Building.objects.filter(owner=nation).annotate(
+        # Annotate to calculate the adjusted ownership percentage
+        adjusted_ownership=ExpressionWrapper(
+            100 - Coalesce(
+                Sum('partialbuildingownership__percentage', filter=~F('partialbuildingownership__partial_owner_abbreviation') == nation.abbreviation),
+                Value(0)
+            ),
+            output_field=FloatField()
+        )
+    )
+    
+    return render(request, 'nation_building_list.html', {'nation': nation, 'buildings': buildings})
