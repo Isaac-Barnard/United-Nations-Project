b2a19ba8ffbdf1b5882d50a278519277bd004a0c
Nelson Cowan <nelson.cowan@hotmail.com>
2025-04-05 18:33:49 -0500

    Ported html/js/css website to django app 'content'
    Halfway finished, still need certain data, maybe put some data in the database?
    Also need players display, work on three.js render first or second hmmm

---
+++ un_project/content/views.py
@@ -0,0 +1,51 @@
+import os
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def Axeman_76(request):
+    return render(request, 'Axeman_76.html')
+
+def OldManReid(request):
+    return render(request, 'Old_Man_Reid.html')
+
+def PapoQuim(request):
+    return render(request, 'Papo_Quim.html')
+
+def Grum(request):
+    return render(request, 'Grum.html')
+
+def UngleNelton(request):
+    return render(request, 'Ungle_Nelton.html')
+
+def minecraft(request):
+    return render(request, 'minecraft.html')
+
+def players(request):
+    return render(request, 'players.html')
+
+def discord(request):
+    return render(request, 'discord.html')
+
+def storytime(request):
+    template = loader.get_template('storytime.html')
+
+    dirlist = os.listdir('staticfiles/stories/')
+    dirlist.sort()
+
+    # Cool one-liner to get the filename and title of the story text files
+    storylist = [{'filename' : filename.split(".txt")[0], 'title' : ' '.join([word.capitalize() for word in filename.split(".txt")[0].split("_")])} for filename in dirlist]
+
+    context = {
+        'story_list': storylist
+    }
+    
+    return HttpResponse(template.render(context, request))
