57d90718766661c9d949f1c1429755b5ee6bb347
Nelson Cowan <nelson.cowan@hotmail.com>
2025-04-05 18:36:19 -0500

    Added 'players_api' app to project, copy of current api for players
    Look into read/write permission locking

---
+++ un_project/players_api/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.TextField()
+    uuid = models.UUIDField(primary_key=True)
+    skin_data = models.TextField(blank=True, null=True)
+    skin_sig = models.TextField(blank=True, null=True)
+    skin_image = models.BinaryField(blank=True, null=True)
+    face_image = models.BinaryField(blank=True, null=True)
+    is_slim = models.BooleanField(default=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
+
+class Inventory(models.Model):
+    inventory_id = models.AutoField(primary_key=True)  # SERIAL PRIMARY KEY
+    uuid = models.ForeignKey(
+        'User',  # Reference the `User` model defined earlier
+        on_delete=models.CASCADE,  # ON DELETE CASCADE
+        db_column='uuid',  # Match the database column name
+    )
+    inventory_type_id = models.IntegerField()
+    slot = models.IntegerField()
+    item_id = models.TextField()
+    amount = models.IntegerField()
+    name = models.TextField()
+    custom_name = models.TextField(blank=True, null=True)
+    enchantments = models.TextField(blank=True, null=True)
+    class Meta:
+        managed = False  # Prevent Django from managing the schema
+        db_table = 'inventory'  # Match the table name in the database
+        unique_together = (('uuid', 'inventory_type_id', 'slot'),)  # UNIQUE constraint
+
+class Players_Online(models.Model):
+    n = models.IntegerField()
+    class Meta:
+        managed = False
+        db_table = 'players_online'
+    
