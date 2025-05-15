1d6d7bcf2d4eaaa398ce73152a9807b6ff55e0be
Isaac Barnard <bigike@werespecialstudios.com>
2025-05-14 19:45:42 -0500

    Created a model for resolutions

--- un_project/un_records_app/models.py
+++ un_project/un_records_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Resolution(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    votes_for = models.PositiveIntegerField()
+    votes_against = models.PositiveIntegerField()
+    void = models.BooleanField(default=False)
+    repealed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.date})"
