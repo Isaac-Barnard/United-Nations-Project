922f7d07679da0f9be6394abe678a94264520d55
Isaac Barnard <bigike@werespecialstudios.com>
2025-05-22 18:32:44 -0500

    Added what nation proposed a resolution

--- un_project/un_records_app/models.py
+++ un_project/un_records_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from un_app.models import Nation
 
 class Resolution(models.Model):
     title = models.CharField(max_length=255, unique=True)
     date = models.DateField()
     votes_for = models.PositiveIntegerField()
     votes_against = models.PositiveIntegerField()
+    proposed_by = models.ForeignKey(Nation, on_delete=models.CASCADE, related_name='proposed_resolution')
     body  = models.TextField()
     void = models.BooleanField(default=False, blank=True)
     repealed = models.BooleanField(default=False, blank=True)
