8f89b41d559cfe872b380e7fb144f48d1e95c5f4
Isaac Barnard <bigike@werespecialstudios.com>
2025-07-08 19:58:04 -0500

    Made ruling properly show actually

--- un_project/un_records_app/views.py
+++ un_project/un_records_app/views.py
@@ -20,6 +20,17 @@ def court_cases(request):
     images_prefetch = Prefetch('images',queryset=CourtCaseArgumentImage.objects.all().order_by('order'))
     arguments_prefetch = Prefetch('case_argued',queryset=CourtCaseArgument.objects.all().order_by('number').prefetch_related(images_prefetch),to_attr='arguments')
     court_cases = (CourtCase.objects.prefetch_related(arguments_prefetch).all().order_by('-date'))
+    
+    # Annotate first special argument in each case
+    for case in court_cases:
+        ruling_time = False
+        for arg in case.arguments:
+            if not ruling_time and arg.argument_type in ["Dissenting Opinion", "Concurring Opinion"]:
+                arg.is_ruling_time = True
+                ruling_time = True
+            else:
+                arg.is_ruling_time = False
+                
     return render(request, 'court_cases.html', {'court_cases': court_cases})
 
 def treaties(request):
