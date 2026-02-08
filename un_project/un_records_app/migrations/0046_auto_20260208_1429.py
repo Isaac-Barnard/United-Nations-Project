acad5daa853a256e8edb29803f8b7515e0d845ad
Isaac Barnard <bigike@werespecialstudios.com>
2026-02-08 15:24:45 -0600

    Try 2

--- un_project/un_records_app/migrations/0046_auto_20260208_1429.py
+++ un_project/un_records_app/migrations/0046_auto_20260208_1429.py
@@ -3,10 +3,24 @@ from django.utils.text import slugify
 
 def populate_courtcase_slugs(apps, schema_editor):
     CourtCase = apps.get_model('un_records_app', 'CourtCase')
+    existing_slugs = set(CourtCase.objects.values_list('slug', flat=True))
+
     for case in CourtCase.objects.all():
-        if not case.slug:
-            case.slug = f"case-{case.case_number}"
-            case.save()
+        # Generate a base slug
+        base_slug = f"case-{case.case_number}"
+        slug = slugify(base_slug)
+        original_slug = slug
+        counter = 1
+
+        # Ensure uniqueness
+        while slug in existing_slugs:
+            counter += 1
+            slug = f"{original_slug}-{counter}"
+
+        # Update slug and track it
+        case.slug = slug
+        case.save()
+        existing_slugs.add(slug)
 
 class Migration(migrations.Migration):
 
