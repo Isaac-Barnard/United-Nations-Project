0b02a841f3d5f839f36ab1cd661033ee8d665034
Isaac Barnard <bigike@werespecialstudios.com>
2026-02-28 14:01:10 -0600

    Added cartography to all menus and updated some formatting

--- un_project/cartography_app/urls.py
+++ un_project/cartography_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from .views import *
-from un_app.views import un_map
+# from un_app.views import un_map
 
 urlpatterns = [
     path('cartography-home', cartography_home, name='cartography_home'),
-    path('map/', un_map, name='interactive_map'),
+    path('interactive-map/', interactive_un_map, name='interactive_un_map'),
+    path('history-maps/', history_maps, name='history_maps'),
 ]
--- un_project/cartography_app/views.py
+++ un_project/cartography_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.db.models import Prefetch
 from .models import *
 
-# Create your views here.
 def cartography_home(request):
     return render(request, 'cartography_home.html')
+
+def interactive_un_map(request):
+    return render(request, 'interactive_un_map.html')
+
+def history_maps(request):
+    return render(request, 'history_maps.html')
